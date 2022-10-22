94ca90f308ec555115298409e82af82684b1f513
MintuGit12 <singhmitu4013@gmail.com>
2022-10-23 01:07:39 +0530

    COMMIT

---
+++ src/test/java/com/crm/practice/ExtendsRepEX1.java
@@ -0,0 +1,17 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ExtendsRepEX1 {
+	@Test
+	public void tc1()
+	{
+		System.out.println("passed-tc1");
+	}
+	@Test
+	public void tc2()
+	{
+		System.out.println("passed-tc2");
+		
+	}
+}
---
+++ src/test/java/com/crm/practice/ExtendsReportEx2.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ExtendsReportEx2 {
+@Test
+public void tc3()
+{
+	System.out.println("passed-tc1");
+}
+
+}
